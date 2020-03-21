6c7c17f334b20a72f6bbe974aa0e55269f10ec4e
munroe1786 <era3munroe@gmail.com>
2020-03-21 10:09:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.random() + 1
+const mod = 16 % 6
+const max = Math.max(1, 4, 6, 8, 12, 20)
